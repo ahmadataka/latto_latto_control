b1230bbde1822e670dc7aa6dafe31dcf3a2183ff
Ahmad Ataka <ataka.ahmad@gmail.com>
2023-01-21 11:01:12 +0700

    Modify action format to enable training. Modify reward function.

--- latto_latto_model.py
+++ latto_latto_model.py
@@ -26,24 +26,30 @@ class LattoLatto(gym.Env):
         self.delta_t = 0.02
         self.friction = 1.0
         self.small_theta = 0.075
+        self.collision_now = 0
+        self.collision_before = 0
 
     def step(self, action):
-        # action = 0
         action = np.float32(np.clip(action, -self.max_action, self.max_action))
         self.act = action
         collision_flag = 0
         self.act = action
         z, z_dot, theta, theta_dot = self.state
 
-        theta_ddot = -(self.g+action)*math.sin(theta)/self.length - self.friction*theta_dot
+        theta_ddot = -(self.g+action[0])*math.sin(theta)/self.length - self.friction*theta_dot
         theta_dot = theta_dot + theta_ddot*self.delta_t
         theta = theta + theta_dot*self.delta_t
-        z_dot = z_dot + action*self.delta_t
+        z_dot = z_dot + action[0]*self.delta_t
         z = z + z_dot*self.delta_t
 
         if(theta<self.small_theta or theta>(math.pi-self.small_theta)):
             theta_dot = theta_dot*(-1)
             collision_flag = 1
+            self.collision_before = self.collision_now
+            if(theta<self.small_theta):
+                self.collision_now = 0
+            else:
+                self.collision_now = 1
 
         self.state = [z, z_dot, theta, theta_dot]
 
@@ -52,15 +58,18 @@ class LattoLatto(gym.Env):
             self.steps_left<0
         )    
         
+        reward = 0
         if not done:
-            if(collision_flag):
+            if(collision_flag and self.collision_now!=self.collision_before):
                 reward = 1
             else:
                 reward = 0
         else:
             reward = 0
+
         if not done:
             self.steps_left = self.steps_left-1
+        
         self.cur_reward = reward
         self.cur_done = done
         return np.array([self.state]), reward, done, {}
@@ -92,7 +101,4 @@ class LattoLatto(gym.Env):
         plt.draw()
         plt.pause(0.001)
         plt.clf()
-        print(pose_point)
-        print(pose_ball_left)
-        print(pose_ball_right)
         print(f'State {self.state}, action: {self.act}, done: {self.cur_done}, reward: {self.cur_reward}')
--- testing_latto.py
+++ testing_latto.py
@@ -8,10 +8,7 @@ observation = env.reset()
 
 for i in range(0,200):
     action = env.action_space.sample()
-    print(env.observation_space.sample().shape)
     observation, reward, terminated, _ = env.step(action)
-    # print("obs:", observation)
-    # print("reward:", reward)
     env.render()
     if terminated:
         print('done')
